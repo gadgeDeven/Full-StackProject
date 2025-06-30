fcde603984c1c46f8e2e391d32d02a0f40a45461
Devendra Gadge <devendragadge80@gmail.com>
2025-07-01 00:04:53 +0530

    Initial Spring Boot setup with landing page and  MySQL connection

---
+++ src/main/java/in/dg/main/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package in.dg.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Will load index.html from templates/
+    }
+}
